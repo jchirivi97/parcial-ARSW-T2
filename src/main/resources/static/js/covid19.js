0a7106b99b7de5360d1ce615774c5906f8ee10c8
jimmy chirivi <jimmychirivi09@gmail.com>
2020-03-26 15:14:16 -0500

    avance front 2

--- src/main/resources/static/js/covid19.js
+++ src/main/resources/static/js/covid19.js
@@ -1,10 +1,40 @@
-var covid19(function(){
+var covid19 = (function(){
 	
 	
 	var infAllCountrys = function(){
 		apiCovid19.allCountrys(mostrarCountrys);
+	
+	}
+	
+	var mostrarCountrys = function(datos){
+		
+		
+		var datosok = JSON.parse(datos);
+		
+		alert(datos);
+		
+		if (datos != null){
+			var objetos = datosok.map(function(datos){
+				
+				return {  country: datos.country,infected: datos.confirmed,
+					deaths: datos.deaths,cured: datos.recovered}				
+				
+			});
+			alert(" datos trnasformados " + objetos);
+		}
 		
 	}
+	
+	
+	/*----------------------------------------------------*/
+	return {
+		
+		allCountrys : infAllCountrys
+		
+	}
+	
+	
+	
 	
 	
 })();
